81b6cac9083971efa19df2a175c981455e2485f7
Vikram kautlya <89295700+Vkautlya99@users.noreply.github.com>
2024-05-11 16:48:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import weblogo from "./images/logo.png"
 function App() {
+  
+  let display = () => {
+    alert("welcome to React.js");
+  }
+  
+  let addData = (num1, num2) => {
+    alert(num1 + num2);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img width={150} src={weblogo} alt="" />
+      {/* For non-parameerized data we will not use the "()" brackets to call the function */}
+      <button className="bg-green-600 rounded-md p-2 w-[60px] h-12 text-white font-bold mb-5" onClick={display}>Click</button> <br />
+
+      {/* For parameerized data we will  use the "()" brackets to call the function */}
+      <button className="bg-green-600 rounded-md p-2 w-[110px] h-12 text-white font-bold" onClick={()=>{addData(22,30)}}>Add Data</button>
     </div>
   );
 }
