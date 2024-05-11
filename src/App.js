9216a2c451592f2aeb95aedcb271a2b4cdcb159b
Vikram kautlya <89295700+Vkautlya99@users.noreply.github.com>
2024-05-11 20:29:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import './App.css';
 
 function App() {
 
-  let [show, setShow] = useState(false);
-
+ 
+  let [password , setPassword] = useState(false)
 
 
 
   
   return (
     <div className="App">
-      <button className="bg-black text-white rounded-md p-2 " onClick={() => setShow(!show)}>
-        {(show) ? "Hide" : "Show"}  
+     
+      <input className='mt-2 bg-slate-300 p-1' type={password ? 'text' : 'password'} />
+      <button className="bg-slate-900 text-white rounded-md p-1 ml-2" onClick = {() => setPassword(!password)}> {password ? "Hide" : 'Show'} </button>
+      <br />
+      <br />
+      <br />
+      <br />
+    
+
       
-      </button>
- 
-      {(show) ? 
-        <p>Hello Vikram</p>
-        :
-        ""
-      }
       
      
     </div>
