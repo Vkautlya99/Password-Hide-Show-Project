66295be117f9e06e5edc94e83f36eaefb55fc42a
Vikram kautlya <89295700+Vkautlya99@users.noreply.github.com>
2024-05-11 20:02:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 
 import { useState } from 'react';
 import './App.css';
-import weblogo from "./images/logo.png"
-function App() {
-  
-  let [count, setCount] = useState(22);
 
-  let display = () => {
-    setCount(count+1)
-    console.log(count);
-  }
+function App() {
+
+  let [show, setShow] = useState(false);
+
+
+
+
   
-  let addData = (num1, num2) => {
-    alert(num1 + num2);
-  }
   return (
     <div className="App">
-      <img width={150} src={weblogo} alt="" />
-
-    {count}
-      {/* For non-parameerized data we will not use the "()" brackets to call the function */}
-      <button className="bg-green-600 rounded-md p-2 w-[60px] h-12 text-white font-bold mb-5" onClick={display}>Click</button> <br />
-
-      {/* For parameerized data we will  use the "()" brackets to call the function */}
-      <button className="bg-green-600 rounded-md p-2 w-[110px] h-12 text-white font-bold" onClick={()=>{addData(22,30)}}>Add Data</button>
+      <button className="bg-black text-white rounded-md p-2 " onClick={() => setShow(!show)}>
+        {(show) ? "Hide" : "Show"}  
+      
+      </button>
+ 
+      {(show) ? 
+        <p>Hello Vikram</p>
+        :
+        ""
+      }
+      
+     
     </div>
   );
 }
